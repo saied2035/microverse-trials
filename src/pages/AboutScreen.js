88ae2aa21678d6ab3e17e0b01c34d8cdb5e238d8
Justin Korkie <justinjkorkie@gmail.com>
2021-11-16 21:04:08 +0200

    Added about page

--- src/pages/AboutScreen.js
+++ src/pages/AboutScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
-
-export default function AboutScreen({navigation}) {
-    return (
-        <div>
-            <p>About</p>
-        </div>
-    )
+import React from "react";
+import './about.css'
+export default function AboutScreen() {
+  return (
+    <div
+      className="flex justify-center items-center about-container"
+    >
+      <iframe
+        width="853"
+        height="480"
+        src={`https://www.youtube.com/embed/f9ip1iSAptQ`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
 }
