efcd4a72f77a0001dfea27b7123a12296356ba3b
Justin Korkie <justinjkorkie@gmail.com>
2021-11-16 19:19:28 +0200

    Navbar update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 //import logo from './logo.svg';
-import './App.css';
-import Navbar from './navbar/navbar'
+import "./App.css";
+import Navbar from "./navbar/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductsScreen from "./pages/ProductsScreen";
+import AboutScreen from "./pages/AboutScreen";
+import HomeScreen from "./pages/HomeScreen";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    // <div className="App">
+    //   <header className="App-header">
+    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    //     <Navbar />
+    //   </header>
+    // </div>
+    <Router>
+      <div>
         <Navbar />
-      </header>
-    </div>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen/>} />
+          <Route exact path="/products" element={<ProductsScreen/>} />
+          <Route exact path="/about" element={<AboutScreen/>} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './navbar.css'
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className="container">
       <nav>
-        <ul>
-          <a className="links" href="/Home">Home</a>
-          <a className="links" href="/About">About</a>
-          <a className="links" href="/Products">Products</a>
-        </ul>
+          <ol className="constainer">
+              <Link className="link" to="/">Home</Link>
+              <Link className="link" to="/about">About</Link>
+              <Link className="link" to="/products">Products</Link>
+          </ol>
       </nav>
     </div>
   );
